71aaec7049f04431b68be963ae0a18e59fe1bd74
The Clout <Theclout@iMac.local>
2017-08-02 23:05:13 -0700

    Add dynamics image

--- Brass-Gym/ThirdViewController.swift
+++ Brass-Gym/ThirdViewController.swift
@@ -14,6 +14,7 @@ class ThirdViewController: UIViewController {
     var randomNumber = 0
     
     @IBOutlet weak var dynamicDisplay: UILabel!
+    @IBOutlet weak var dynamicsImage: UIImageView!
     
   
     
@@ -25,13 +26,23 @@ class ThirdViewController: UIViewController {
     @IBAction func getDynamicScheme(_ sender: Any) {
         randomNumber = Int(arc4random_uniform(UInt32(schemes.count)))
         dynamicDisplay.text = schemes[randomNumber]
+        showImage()
     }
     
-
+    func showImage(){
+        if dynamicDisplay.text == schemes[5]{
+            dynamicsImage.image = #imageLiteral(resourceName: "Diamond-bowtie")
+            dynamicsImage.isHidden = false
+        }else{
+            dynamicsImage.isHidden = true
+        }
+    }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Do any additional setup after loading the view.
     }
